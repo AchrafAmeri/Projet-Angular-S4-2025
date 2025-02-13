a0417300d445f425850c364d423a75b558c0005f
IUT BLAGNAC <noreply@univ-tlse2.fr>
2025-02-13 12:14:23 +0100

    delete + css

--- src/app/menu/menu-item/menu-item.component.ts
+++ src/app/menu/menu-item/menu-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Menu, StatutMenu } from '../../models/menu';
+import { Router } from '@angular/router';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-menu-item',
@@ -10,4 +12,26 @@ export class MenuItemComponent {
   @Input()
   public menu: Menu = new Menu();
   readonly statutMenu = StatutMenu;
+
+  constructor(
+    private menuService: MenuService,
+    private router: Router,
+  ) {}
+
+  public onDelete(): void {
+    let ObservableAction
+    ObservableAction = this.menuService.deleteMenu(this.menu)
+    ObservableAction.subscribe({
+      next: menu => {
+        console.log("Suppression OK : ", menu)
+        this.router.navigateByUrl("/").then(
+          ()=>this.router.navigateByUrl("/menus") 
+          )
+      },
+      error: err => {
+        console.log("ERREUR DELETE ", err)
+      }
+    })
+  }
+  
 }
--- src/app/plat/plat-item/plat-item.component.ts
+++ src/app/plat/plat-item/plat-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Plat } from '../../models/plat';
+import { PlatService } from '../../services/plat.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-plat-item',
@@ -9,4 +11,25 @@ import { Plat } from '../../models/plat';
 export class PlatItemComponent {
   @Input()
   public plat: Plat = new Plat();
+
+  constructor(
+    private platService: PlatService,
+    private router: Router,
+  ) {}
+
+  public onDelete(): void {
+    let ObservableAction
+    ObservableAction = this.platService.deletePlat(this.plat)
+    ObservableAction.subscribe({
+      next: menu => {
+        console.log("Suppression OK : ", menu)
+        this.router.navigateByUrl("/").then(
+          ()=>this.router.navigateByUrl("/menus") 
+          )
+      },
+      error: err => {
+        console.log("ERREUR DELETE ", err)
+      }
+    })
+  }
 }
